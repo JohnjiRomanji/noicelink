694cab504ecdd84820eab6182d9681bb1a5cea5c
JohnjiRomanji <69640272+JohnjiRomanji@users.noreply.github.com>
2021-03-21 08:46:08 -0400

    Update __init__.py

--- noicepy/__init__.py
+++ noicepy/__init__.py
@@ -23,4 +23,13 @@ SOFTWARE.
 
 '''
 
+
+'''
+LOOK AT THE DOCS 
+PLEASE LOOK AT THE DOCS
+https://johnjiromanji.github.io/noicepy
+
+Join the noice.link discord server (on the website) for support
+'''
+
 from .noicepy import Link
