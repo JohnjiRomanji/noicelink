a230c051a1627fcd416ccb9975c941a6968f354f
JohnjiRomanji <69640272+JohnjiRomanji@users.noreply.github.com>
2021-03-22 07:57:46 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setup(
     install_requires=['requests'],
     # *strongly* suggested for sharing
     version='1.0',
-    project_urls=
-        docs="https://johnjiromanji.github.io/noicepy"
+    
+    docs="https://johnjiromanji.github.io/noicepy"
     # The license can be anything you like
     license='MIT',
     description='A simple and easy to use Python wrapper for the noice.link API',
